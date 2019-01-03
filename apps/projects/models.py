e74ed2f692b9bb9b7c53c6b2992862024d2e551e
Xpitz <Xpitz@protonmail.com>
2019-01-03 11:23:33 +0800

    update style

---
+++ apps/projects/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Module(models.Model):
+    name = models.CharField(max_length=32, verbose_name="模块名")
+    description = models.TextField(verbose_name="描述")
+    port = models.IntegerField(verbose_name="端口", blank=True, null=True)
+    charger = models.CharField(default="", max_length=64, blank=True, verbose_name="负责人")
+    package_name = models.CharField(max_length=32, blank=True, null=True, verbose_name="包名")
+    project_name = models.CharField(max_length=32, verbose_name="项目名")
