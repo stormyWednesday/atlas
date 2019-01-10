edf3c75e596b46422bb716f19cf818e66933f1be
jiangyangyang <yangyang.jiang@jiamiantech.com>
2019-01-10 14:56:01 +0800

    新增删改功能

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -7,4 +7,6 @@ app_name = 'projects'
 urlpatterns = [
     path('project/list/', views.ProjectListView.as_view(), name='project-list'),
     path('project/create/', views.ProjectCreateView.as_view(), name='project-create'),
+    path('project/delete/<int:pk>/', views.ProjectDelete.as_view(), name='project-delete'),
+    path('project/update/<int:pk>/', views.ProjectUpdate.as_view(), name='project-update'),
 ]
--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView, CreateView
+from django.views.generic import ListView, TemplateView, CreateView, DeleteView, UpdateView
 from django.urls import reverse_lazy
+from django.contrib import messages
 
 from apps.projects.models import Module
 from apps.projects.forms import ProjectForm
 
+
+
+
 # Create your views here.
 
 
-class ProjectListView(TemplateView):
+class ProjectListView(ListView):
+    model = Module
     template_name = 'project/list.html'
+    context_object_name = 'project_obj'
+
 
 
 class ProjectCreateView(CreateView):
@@ -18,4 +25,46 @@ class ProjectCreateView(CreateView):
     template_name = 'project/create.html'
     context_object_name = 'project_obj'
     success_url = reverse_lazy('projects:project-list')
+
+
+class ProjectDelete(DeleteView):
+    model = Module
+    template_name = 'project/delete.html'
+    context_object_name = 'project_obj'
+    success_url = reverse_lazy('projects:project-list')
+
+
+class ProjectUpdate(UpdateView):
+    model = Module
+    form_class = ProjectForm
+    template_name = 'project/update.html'
+    context_object_name = 'project_obj'
+    success_url = reverse_lazy('projects:project-list')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
 
